07974f8e2a7364007c2fecbfd7a347058448b009
hillt03 <hillt03@yahoo.com>
2019-08-22 18:22:31 -0400

    Added support for wildcard filepaths

--- PyEZBackup/pyezbackup.py
+++ PyEZBackup/pyezbackup.py
@@ -2,17 +2,18 @@ import os
 from distutils.dir_util import copy_tree
 from distutils.file_util import copy_file
 import argparse
+import sys
 
 
 class PyEZBackup:
     def __init__(self, filepath_file, backup_location):
-        self.filepath_list = []
+        self.filepath_list = [] # Stores filepaths that will be backed up
         self.filenames = []  # Excludes path apart from filename
-        self.filepath_file = filepath_file
-        self.backup_location = backup_location
-        self.main_dir = os.getcwd()
+        self.filepath_file = filepath_file # Path to the file containing filepaths to backup
+        self.backup_location = backup_location # Path to where folders/files will be backed up
+        self.main_dir = os.getcwd() # Stores PyEZBackup location
         if not os.path.exists(self.backup_location):
-            os.mkdir(backup_location)
+            os.mkdir(backup_location) # Creates the backup location if it doesn't exist
 
     def read_filepaths(self):
         """
@@ -24,17 +25,24 @@ class PyEZBackup:
         print("Filepath backup list:")
         for file in self.filepath_list:
             if "." in file:
-                print(f"File: {self.get_filename(file)}")
+                if "*" in file: # Check for wildcard paths
+                    self.add_wildcard_filepaths(file)
+                else:
+                    print(f"File: {self.get_filename(file)}")
             else:
                 print(f"Folder: {self.get_filename(file)}")
         print("=============================================")
         if not self.filepath_list:
-            raise Exception("Empty filepath list.")
+            print("Empty filepath list.")
+            sys.exit(0)
         for file in self.filepath_list:
             self.filenames.append(self.get_filename(file))
         return self.filepath_list
 
     def backup_filepaths(self):
+        """
+        Backs up each file in self.filepath_list to self.backup_location.
+        """
         for filepath in self.filepath_list:
             print("Backing up " + self.get_filename(filepath))
             if os.path.isfile(filepath):
@@ -43,8 +51,17 @@ class PyEZBackup:
                 self.copytree(filepath, self.backup_location)
             else:
                 print(f"Invalid filepath: {filepath}")
-
         self._check_backup()
+
+    def add_wildcard_filepaths(self, filepath):
+        extension = os.path.splitext(self.get_filename(filepath))[1]
+        for dir_file in self.listdir_fullpath(os.path.dirname(filepath)):
+            if dir_file.endswith(extension):
+                self.filepath_list.append(dir_file)
+        self.filepath_list.remove(filepath)
+
+    def listdir_fullpath(self, d):
+        return [os.path.join(d, f) for f in os.listdir(d)]
 
     def copytree(self, src, dst):
         """
@@ -58,9 +75,17 @@ class PyEZBackup:
         copy_tree(src, dst + "/" + folder)
 
     def get_filename(self, filepath):
+        """
+        Removes path from filename.
+        """
         return os.path.basename(filepath)
 
     def _check_backup(self):
+        """
+        Ensures backup completed successfully.
+        """
+        print("---------------------------------------------")
+        print("Backup results:")
         os.chdir(self.backup_location)
         backup_list = os.listdir()
         backup_failed = False
@@ -70,6 +95,7 @@ class PyEZBackup:
                 backup_failed = True
         if not backup_failed:
             print("Backup successful.")
+        print("---------------------------------------------")
         os.chdir(self.main_dir)
 
 
